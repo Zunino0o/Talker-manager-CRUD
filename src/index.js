af169d09a8b28ead9bffe15c1f6d282381b39a30
Lucas Zunino <zunino10@hotmail.com>
2023-05-17 16:14:00 -0300

    req01 DONE!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,14 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) => {
+  const data = await fs.readFile('./src/talker.json', 'utf-8');
+  const talkersList = JSON.parse(data);
+  console.log(data);
+  console.log(talkersList);
+  res.status(HTTP_OK_STATUS).json(talkersList);
+})
+
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log(`Online on port ${PORT}`);
 });
