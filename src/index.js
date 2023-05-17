7978bd0115a51aebc5d66c55966415dc290f15d5
Lucas Zunino <zunino10@hotmail.com>
2023-05-17 20:52:48 -0300

    req02 DONE!

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-const express = require('express');
-const fs = require('fs').promises;
+const express = require("express");
+const fs = require("fs").promises;
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || "3001";
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (req, res) => {
-  const data = await fs.readFile('./src/talker.json', 'utf-8');
+app.get("/talker", async (req, res) => {
+  const data = await fs.readFile("./src/talker.json", "utf-8");
   const talkersList = JSON.parse(data);
-  console.log(data);
-  console.log(talkersList);
+  // console.log(data);
+  // console.log(talkersList);
   res.status(HTTP_OK_STATUS).json(talkersList);
-})
+});
+
+app.get("/talker/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await fs.readFile("./src/talker.json", "utf-8");
+  const talkersList = JSON.parse(data);
+  const [filter] = talkersList.filter((t) => t.id === +id);
+  // console.log(talkersList);
+  // console.log(filter);
+  if (filter) {
+    res.status(HTTP_OK_STATUS).json(filter);
+  } else {
+    res.status(404).json({
+      message: "Pessoa palestrante não encontrada",
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Online on port ${PORT}`);
