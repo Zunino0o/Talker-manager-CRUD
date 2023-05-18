80e41c24ccead2867a0bd90595eb77b390bb3147
Lucas Zunino <zunino10@hotmail.com>
2023-05-18 00:55:56 -0300

    req04 DONE!

--- src/index.js
+++ src/index.js
@@ -7,9 +7,45 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-const randomId = (length) => Math.random()
+// Gera um ID aleatorio
+const randomId = (length) =>
+  Math.random()
     .toString(36)
     .substring(2, length + 2);
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const emailRegex = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email);
+  // console.log(emailRegex);
+  if (!email) {
+    return res.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+  if (!emailRegex) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  return next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+  const checkLength = password.length > 5;
+  // console.log(checkLength, password);
+  if (password && !checkLength) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+  return next();
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -43,7 +79,7 @@ app.get('/talker/:id', async (req, res) => {
 });
 
 // REQ 3:
-app.post('/login', async (req, res) => {
+app.post('/login', validateEmail, validatePassword, async (req, res) => {
   // const { email, password } = req.body;
   const token = randomId(8) + randomId(8);
   res.status(200).json({ token });
